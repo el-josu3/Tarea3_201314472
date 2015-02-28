ae01c295bd65890790d5ce3b9634a3f73a314ad6
el-josu3 <josu3.ich@gmail.com>
2015-02-28 15:23:57 -0800

    Tercer Tarea
    Codigo para numeros pares e impares

---
+++ Tarea3/src/Tarea3_201314472/Pares.java
@@ -0,0 +1,26 @@
+package Tarea3_201314472;
+import java.*;
+import java.util.Scanner;
+
+public class Pares {
+
+	public static void main(String[] args) {
+		int num=0;
+		System.out.println("Porfavor ingrese un numero");
+		Scanner j=new Scanner(System.in);
+		num=j.nextInt();
+		if(num%2!=0){
+			System.out.println("El numero que ingreso es impar");
+		}
+			else{
+				System.out.println("El numero ingresado es par");
+			}
+				
+	}
+}
+
+			
+		
+
+
+
